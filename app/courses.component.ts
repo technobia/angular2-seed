33e6149bb02deafb7c82e19cf127866ef2802463
Binh <technobia@gmail.com>
2016-07-21 11:30:04 +0700

    repeat in component

--- app/courses.component.ts
+++ app/courses.component.ts
@@ -5,8 +5,17 @@ import {Component} from 'angular2/core'
 
 @Component({
     selector: 'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+        <h2>Courses</h2>
+        {{title}}
+        <ul>
+            <li *ngFor="#course of courses">
+            {{course}}
+            </li>
+        </ul>
+        `
 })
 export class CoursesComponent {
-
+    title = 'The title of courses page';
+    courses = ['Courses1', 'Courses2', 'Courses3']
 }

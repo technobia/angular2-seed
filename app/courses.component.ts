bef15797098db9b02ee952732693596a39c9c760
Binh <technobia@gmail.com>
2016-07-21 11:24:37 +0700

    courses component

---
+++ app/courses.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by apium on 21/07/2016.
+ */
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'courses',
+    template: '<h2>Courses</h2>'
+})
+export class CoursesComponent {
+
+}
